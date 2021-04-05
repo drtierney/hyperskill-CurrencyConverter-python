024c4c17fcb16457c3903a385d0dd81cd7fd3533
drtierney <d.r.tierney@hotmail.co.uk>
2021-04-05 17:10:44 +0100

    Stage #3 completed
    Stage 3/6: More interaction

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,6 +1,3 @@
-conicoins = int(input())
-dollars = conicoins * 100
-
-print(f"I have {conicoins} conicoins.")
-print(f"{conicoins} conicoins cost {dollars} dollars.")
-print("I am rich! Yippee!")
+conicoins = float(input("Please, enter the number of conicoins you have: "))
+exchange_rate = float(input("Please, enter the exchange rate: "))
+print("The total amount of dollars: {0}".format(round(conicoins * exchange_rate,2)))
