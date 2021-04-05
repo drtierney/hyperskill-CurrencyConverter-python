02b08df57988b5cb68fde44547a9257ec6b6a1be
drtierney <d.r.tierney@hotmail.co.uk>
2021-04-05 19:12:11 +0100

    Stage #4 completed
    Stage 4/6: Going global

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,3 +1,20 @@
-conicoins = float(input("Please, enter the number of conicoins you have: "))
-exchange_rate = float(input("Please, enter the exchange rate: "))
-print("The total amount of dollars: {0}".format(round(conicoins * exchange_rate,2)))
+
+currencies = {
+    'RUB': {'name': 'Russian Ruble', 'rate': 2.98},
+    'ARS': {'name': 'Argentine Peso', 'rate': 0.82},
+    'HNL': {'name': 'Honduran Lempira', 'rate': 0.17},
+    'AUD': {'name': 'Australian Dollar', 'rate': 1.9622},
+    'MAD': {'name': 'Moroccan Dirham', 'rate': 0.208},
+}
+
+
+def conicoin_rates(**currency_dict):
+    conicoins = float(input())
+
+    for code in currencies.keys():
+        amount = conicoins * currencies[code]['rate']
+        print(f"I will get {round(amount, 2)} {code} from the sale of {conicoins} conicoins.")
+
+
+if __name__ == '__main__':
+    conicoin_rates(**currencies)
